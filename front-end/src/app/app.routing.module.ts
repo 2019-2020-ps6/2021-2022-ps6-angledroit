88702a0e4bd0b391e0f0e25e452a0cde7dac26ce
Anasselayachi <anasselyachi2002@gmail.com>
2022-03-26 09:27:08 +0100

    Revert "add connection page"
    This reverts commit 4fbbc0ee94bca4c6dc71ef2faa682c73ad3bc422.

--- front-end/src/app/app.routing.module.ts
+++ front-end/src/app/app.routing.module.ts
@@ -3,13 +3,11 @@ import { RouterModule, Routes } from '@angular/router';
 import { QuizListComponent } from './quizzes/quiz-list/quiz-list.component';
 import { EditQuizComponent } from './quizzes/edit-quiz/edit-quiz.component';
 import { UserListComponent } from './users/user-list/user-list.component';
-import { ConnectionComponent } from './connection/connection.component';
 
 const routes: Routes = [
     {path: 'user-list', component: UserListComponent},
     {path: 'quiz-list', component: QuizListComponent},
     {path: 'edit-quiz/:id', component: EditQuizComponent},
-    {path: 'connection', component: ConnectionComponent},
     { path: '', redirectTo: '/quiz-list', pathMatch: 'full' },
 ];
 
