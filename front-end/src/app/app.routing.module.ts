51804d6bc47cc3020dcb38249e37cbf68de21a2b
Anasselayachi <anasselyachi2002@gmail.com>
2022-03-28 18:15:39 +0200

    Page d'accueil/choix de thème

--- front-end/src/app/app.routing.module.ts
+++ front-end/src/app/app.routing.module.ts
@@ -3,11 +3,13 @@ import { RouterModule, Routes } from '@angular/router';
 import { QuizListComponent } from './quizzes/quiz-list/quiz-list.component';
 import { EditQuizComponent } from './quizzes/edit-quiz/edit-quiz.component';
 import { UserListComponent } from './users/user-list/user-list.component';
+import { AccueilComponent } from './accueil/accueil.component';
 
 const routes: Routes = [
     {path: 'user-list', component: UserListComponent},
     {path: 'quiz-list', component: QuizListComponent},
     {path: 'edit-quiz/:id', component: EditQuizComponent},
+    {path: 'accueil', component: AccueilComponent},
     { path: '', redirectTo: '/quiz-list', pathMatch: 'full' },
 ];
 
