3524f4fecbb86b4c005f512a47808243702bb46c
Anasselayachi <anasselyachi2002@gmail.com>
2022-03-29 10:48:29 +0200

    #2 mise en forme page quiz

--- front-end/src/app/quiztest/quiztest.component.ts
+++ front-end/src/app/quiztest/quiztest.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import {Answer, Question} from '../../models/question.model';
+import {Quiz} from '../../models/quiz.model';
+import {ActivatedRoute} from '@angular/router';
+import {QuizService} from '../../services/quiz.service';
 
 @Component({
   selector: 'app-quiztest',
@@ -6,10 +10,21 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./quiztest.component.scss']
 })
 export class QuiztestComponent implements OnInit {
+  indexQuiz = 0;
+  public question: Question;
+  public answer: Answer;
+  public quiz: Quiz;
+  id: string;
 
-  constructor() { }
+  constructor(private route: ActivatedRoute, private quizService: QuizService) {
+    this.quizService.quizSelected$.subscribe((quiz) => (this.quiz = quiz));
+  }
 
   ngOnInit(): void {
+    this.id = this.route.snapshot.paramMap.get('id');
+    this.quizService.setSelectedQuiz(this.id);
   }
+
+
 
 }
